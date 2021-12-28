3d47a4f249fa4cc32f126daaa371137e4d2470fd
Arailson Santos <arailson.vieira@gmail.com>
2021-12-28 19:18:44 -0300

    Projeto guiado completo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,44 @@
-var people = [
-    {
-        name: 'Arailson Santos de Aragão',
-        tel:  '+55 (11) 94985-6602',
-        xp: true
-    },
-    {
-        name: 'Rosangela Bayma de Oliveira',
-        tel:  '+55 (11) 94985-6602',
-        xp: false
-    },
-    {
-        name: 'Manuella Bayma dos Santos',
-        tel:  '+55 (11) 94985-6602',
-        xp: false
-    },
-    {
-        name: 'Pietro Bayma dos Santos',
-        tel:  '+55 (11) 94985-6602',
-        xp: false
-    },
-];
-
-
-for (person in people){
-    document.querySelector('table.lista tbody').innerHTML+=`
-    <tr style= "background-color: ${((person % 2 == 0) ? '#fff' : '#ddd')}">
-        <td>
-            ${people[person].name}
-        </td>
-        <td>
-            ${people[person].tel}
-        </td>
-        <td>
-            ${(people[person].xp ? '<strong style= "color: green"> Sim </strong>' : '<strong style= "color: red"> Não </strong>')}
-        </td>
-        <td>
-            <button> Alterar </button>
-        </td>
-
-    </tr>`
+var peopleRaw = localStorage.getItem('people')
+if (peopleRaw != null) {
+    var people = JSON.parse(peopleRaw)
+} 
+else {
+    var people = [];
 }
+
+
+function desenhaTabela() {
+
+    currentLines = [...document.querySelectorAll('table.lista tbody .dinamic-content')];
+    currentLines.forEach((element) => {
+        element.remove()
+    })
+
+    for (person in people) {
+        document.querySelector('table.lista tbody').innerHTML += `
+        <tr class= "dinamic-content" style= "background-color: ${((person % 2 == 0) ? '#fff' : '#ddd')}">
+            <td>
+                ${people[person].name}
+            </td>
+            <td>
+                ${people[person].tel}
+            </td>
+            <td>
+                ${(people[person].xp ? '<strong style= "color: green"> Sim </strong>' : '<strong style= "color: red"> Não </strong>')}
+            </td>
+            <td>
+                <button onclick= "deleteUser(${person})"> Excluir </button>
+                <a href="./src/form.html?person=${person}">Editar</a>
+            </td>
+    
+        </tr>`
+    }
+}
+function deleteUser(p) {
+    people.splice(p, 1);
+    desenhaTabela();
+    localStorage.setItem('people', JSON.stringify(people))
+}
+
+
+desenhaTabela()
